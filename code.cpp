45dceb31c9d326449465155eb7e016b589149446
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2024-12-31 11:32:24 +0530

    code

--- code.cpp
+++ code.cpp
@@ -10,12 +10,20 @@ int main(){
     // cin >> age ;
     // cout << "Your age is : " << age << endl;
 
-    int a = 5 , b = 10;
-    cout << "Sum : " << (a+b) << endl;
-    cout << "Difference : " << (a-b) << endl;
-    cout << "Product : " << (a*b) << endl;
-    cout << "Division : " << (a/b) << endl;
-    cout << "modulo : " << (a%b) << endl;
+    // int a = 5 , b = 10;
+    // cout << "Sum : " << (a+b) << endl;
+    // cout << "Difference : " << (a-b) << endl;
+    // cout << "Product : " << (a*b) << endl;
+    // cout << "Division : " << (a/b) << endl;
+    // cout << "modulo : " << (a%b) << endl;
+
+    // int a = 12 , b =25 ;
+    // cout << (a < b ) << endl;
+    // cout << (a > b ) << endl;
+    // cout << (a == b)  << endl;
+    // cout << (a != b)  << endl;
+    // cout << (a <= b)  << endl;
+    // cout << (a >= b)  << endl;
 
     return 0;
 

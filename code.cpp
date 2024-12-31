23b7a789fec70ebe507371bfb6469e9fbaea4777
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2024-12-31 11:26:38 +0530

    code

--- code.cpp
+++ code.cpp
@@ -3,8 +3,20 @@ using namespace std;
 
 
 int main(){
+    
+    // cout << "Hello User" << endl ;
+    // int age ;
+    // cout << "Enter your age : " ;
+    // cin >> age ;
+    // cout << "Your age is : " << age << endl;
 
-    cout << "Hello World" << endl ;
+    int a = 5 , b = 10;
+    cout << "Sum : " << (a+b) << endl;
+    cout << "Difference : " << (a-b) << endl;
+    cout << "Product : " << (a*b) << endl;
+    cout << "Quotient : " << (a/b) << endl;
+    cout << "Remainder : " << (a%b) << endl;
+
     return 0;
 
 }

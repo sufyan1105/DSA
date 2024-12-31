0b8f037c13e9ed13da3ed83f135be38aab1aec52
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2024-12-31 11:43:11 +0530

    sum

---
+++ sum.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a , b , sum;
+    cout << "Enter number 1 : " ;
+    cin >> a ;
+    cout << "Enter number 2 : " ;
+    cin >> b ;
+    sum = a + b ;
+    cout << "Sum of " << a << " and " << b << " is " << sum << endl;
+    return 0;
+}
+

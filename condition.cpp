bd37a0334ea8fc6944d1ca230e6427c7c12c5003
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-01 10:47:48 +0530

    Conditions and loop

---
+++ condition.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 20;
+
+    if (n >= 18) {
+        cout << "You are an adult." << endl;
+    }
+    else {
+        cout << "You are a minor." << endl;
+    }
+}

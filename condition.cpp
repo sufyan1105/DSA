211bf055b4d65d6a7f2d7ea4cdaab2fa41139c71
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-01 11:08:50 +0530

    condition

--- condition.cpp
+++ condition.cpp
@@ -24,19 +24,42 @@ int main() {
     //     cout << "Number is odd." << endl;
     // }
 
-    int marks;
-    cout << "Enter your marks : ";
-    cin >> marks;
+    // int marks;
+    // cout << "Enter your marks : ";
+    // cin >> marks;
 
-    if (marks >= 90 ) {
-        cout << "Grade A" << endl;
-    }
-    else if (marks >= 80 && marks < 90) {
-        cout << "Grade B" << endl;
-    }
-    else {
-        cout << "Grade C" << endl;
-    }
+    // if (marks >= 90 ) {
+    //     cout << "Grade A" << endl;
+    // }
+    // else if (marks >= 80 && marks < 90) {
+    //     cout << "Grade B" << endl;
+    // }
+    // else {
+    //     cout << "Grade C" << endl;
+    // }
+
+    // char ch ;
+    // cout << "Enter a character : " ;
+    // cin >> ch ;
+
+    // if (ch >= 'a' && ch <= 'z' ) {
+    //     cout << "Character is a small letter." << endl;
+    // }
+    // else {
+    //     cout << "Character is a capital letter." << endl;
+    // }
+
+    // char ch;
+    // cout << "Enter a character : ";
+    // cin >> ch;
+
+    // if (ch >= 65 && ch <= 90) {
+    //     cout << "Character is a capital letter." << endl;
+    // } else {
+    //     cout << "Character is a small letter." << endl;
+    // }
+
+    
 
     return 0;
 }

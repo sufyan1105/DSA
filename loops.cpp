22f6c20f78b47cd9ace2ece6a880b38a8dbcc8c1
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-01 11:44:50 +0530

    loops

--- loops.cpp
+++ loops.cpp
@@ -3,12 +3,25 @@ using namespace std;
 
 int main(){
 
-    int i = 1;
+    // int i = 1;
 
-    while (i <= 10) {
-        cout << i << " ";
-        i++;
+    // while (i <= 10) {
+    //     cout << i << " ";
+    //     i++;
+    // }
+
+    // int n = 10;
+    
+    // for ( int i = 1 ; i <= n ; i++) {
+    //     cout << i << " ";
+    // }
+    int sum = 0;
+    int n = 5;
+    for ( int i = 1 ; i <= n; i++){
+        sum += i;
     }
+    cout << sum;
+
     cout << endl;
     return 0;
 }

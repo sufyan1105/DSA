34280a402b7c2b1d1325087cac6a407e76bf260f
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-01 12:03:04 +0530

    loops

--- loops.cpp
+++ loops.cpp
@@ -15,12 +15,42 @@ int main(){
     // for ( int i = 1 ; i <= n ; i++) {
     //     cout << i << " ";
     // }
-    int sum = 0;
-    int n = 5;
-    for ( int i = 1 ; i <= n; i++){
-        sum += i;
-    }
-    cout << sum;
+    // int sum = 0;
+    // int n = 5;
+    // for ( int i = 1 ; i <= n; i++){
+    //     sum += i;
+    // }
+    // cout << sum;
+
+    // int n = 10;
+    // int sum = 0;
+
+    // for ( int i = 1; i <= n; i++) {
+    //     if ( i % 2 != 0){
+    //         sum += i;
+    //     }
+    // }
+    // cout << sum ;
+
+    // int n = 5;
+    // int sum =0;
+    // int i = 1;
+
+    // while (i <= n) {
+    //     if (i % 2 != 0){
+    //         sum += i;
+    //     }
+    //     i++;
+    // }
+    // cout << sum << " " ;
+    // int sum = 0;
+    // int n = 5;
+    // for (int i = 1; i <=n ; i++){
+    //     if (i % 2 == 0){
+    //         sum +=i;
+    // }
+    // }
+    // cout << sum << " ";
 
     cout << endl;
     return 0;

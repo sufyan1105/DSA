42120b3a2f143b44948643ed1b68fcfcabf9a66c
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-02 12:09:44 +0530

    pattern

--- patterns.cpp
+++ patterns.cpp
@@ -118,19 +118,50 @@ int main(){
     //     cout << endl;
     // }
 
+    // int n = 4;
+    // for(int i=0; i<n; i++){
+    //     // Spaces
+    //     for (int j=0; j<n-i-1; j++){
+    //         cout <<  " ";
+    //     }
+    //     // num 1
+    //     for (int j =1; j<= i+1 ; j++){
+    //         cout << j ;
+    //     }
+    //     // num 2
+    //     for (int j = i; j>0; j--){
+    //         cout << j;
+    //     }
+    //     cout << endl;
+    // }
+
     int n = 4;
-    for(int i=0; i<n; i++){
-        // Spaces
-        for (int j=0; j<n-i-1; j++){
-            cout <<  " ";
+    for (int i =0; i<n; i++){
+        for (int j =0 ; j<n-i-1; j++ ){
+            cout << " ";
         }
-        // num 1
-        for (int j =1; j<= i+1 ; j++){
-            cout << j ;
+        cout << "*";
+
+        if( i != 0){
+            for (int j =0 ; j<2*i-1; j++ ){
+                cout << " ";
+            }
+            cout << "*";
         }
-        // num 2
-        for (int j = i; j>0; j--){
-            cout << j;
+        cout << endl;
+    }
+
+    for (int i = 0; i<n-1; i++){
+        for (int j =0 ; j<i+1; j++ ){
+            cout << " ";
+        }
+        cout << "*";
+
+        if (i != n-2){
+            for (int j =0 ; j<2*(n-i)-5; j++ ){
+            cout << " ";
+        }
+            cout << "*";
         }
         cout << endl;
     }

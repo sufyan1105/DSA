7a358aff5e1cfa2afd0228bb9aebc9f15ceafb4c
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-02 11:29:50 +0530

    pattern

--- patterns.cpp
+++ patterns.cpp
@@ -59,15 +59,39 @@ int main(){
     //     cout << endl;
     // }
     
+    // int n = 4;
+    // for(int i=0; i<n; i++){
+    //     for (int j=0; j<i+1; j++){
+    //         cout << (i+1) ;
+    //     }
+    //     cout << endl;
+    // }
+
+    // int n = 4;
+    // char ch = 'A';
+    // for(int i=0; i < n; i++){
+    //     for (int j=0; j < i+1; j++){
+    //         cout << ch  ;
+    //     }
+    //     cout << endl;
+    //     ch++;
+    // }
+
+    // int n = 4;
+    // for(int i=0; i<n; i++){
+    //     for (int j=1; j<= i+1; j++){
+    //         cout << j << " " ;
+    //     }
+    //     cout << endl;
+    // }
+    
     int n = 4;
-    int num = 1;
     for(int i=0; i<n; i++){
-        for (int j=0; j<i+1; j++){
-            cout << (i+1) ;
+        for (int j=i+1; j>0; j--){
+            cout << j << " ";
         }
         cout << endl;
     }
-
 
 
     return 0;

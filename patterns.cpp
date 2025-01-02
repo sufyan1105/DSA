b3ad81dd0e562c8450ef159766e042dc6351a3de
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-02 11:46:45 +0530

    patterns

--- patterns.cpp
+++ patterns.cpp
@@ -85,10 +85,52 @@ int main(){
     //     cout << endl;
     // }
     
+    // int n = 4;
+    // for(int i=0; i<n; i++){
+    //     for (int j=i+1; j>0; j--){
+    //         cout << j << " ";
+    //     }
+    //     cout << endl;
+    // }
+
+    // int n = 4;
+    // int num = 1;
+    // for(int i=0; i<n; i++){
+    //     for (int j=i+1; j>0; j--){
+    //         cout << num << " ";
+    //         num++;
+    //     }
+    //     cout << endl;
+    // }
+
+    // int n = 4;
+    
+    // for(int i=0; i<n; i++){
+
+    //     for (int j=0; j<i; j++){
+    //         cout << " ";
+    //     }
+
+    //     for (int j = 0; j<n-i; j++){
+    //         cout << (i+1);
+    //     }
+
+    //     cout << endl;
+    // }
+
     int n = 4;
     for(int i=0; i<n; i++){
-        for (int j=i+1; j>0; j--){
-            cout << j << " ";
+        // Spaces
+        for (int j=0; j<n-i-1; j++){
+            cout <<  " ";
+        }
+        // num 1
+        for (int j =1; j<= i+1 ; j++){
+            cout << j ;
+        }
+        // num 2
+        for (int j = i; j>0; j--){
+            cout << j;
         }
         cout << endl;
     }

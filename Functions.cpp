db83e2100e8a8eee74254e2628bf52708dd112cd
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-03 14:45:38 +0530

    functions

--- Functions.cpp
+++ Functions.cpp
@@ -7,18 +7,18 @@ using namespace std;
 // }
 
 // Function for minimum of 2 numbers
-int min(int a, int b){
-    if (a>b) {
-        return b;
-    }
-    else {
-        return a;
-    }
-}
+// int min(int a, int b){
+//     if (a>b) {
+//         return b;
+//     }
+//     else {
+//         return a;
+//     }
+// }
 
 int main(){
 
     // cout << sum(12,13) << endl ;
-    cout << min(12,13) << endl ;
+    // cout << min(12,13) << endl ;
     return 0;
 }

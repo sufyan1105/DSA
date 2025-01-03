5215f6740b4e61d5a5b1d8e719cee8fa8102ebe0
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-03 14:44:35 +0530

    functions

--- Functions.cpp
+++ Functions.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int sum(int a , int b){
-    return a + b;
+// Function for sum of 2 numbers
+// int sum(int a , int b){
+//     return a + b;
+// }
+
+// Function for minimum of 2 numbers
+int min(int a, int b){
+    if (a>b) {
+        return b;
+    }
+    else {
+        return a;
+    }
 }
 
 int main(){
 
-    cout << sum(12,13) << endl ;
+    // cout << sum(12,13) << endl ;
+    cout << min(12,13) << endl ;
     return 0;
 }

e5da6d90541460715cf4177d454875f7923e45ed
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-04 15:29:15 +0530

    Binary

--- Binary.cpp
+++ Binary.cpp
@@ -1,23 +1,42 @@
 #include <iostream>
 using namespace std;
 
-int decToBinary(int decNum){
+// Decimal to Binary
+// int decToBinary(int decNum){
+//     int ans = 0;
+//     int pow = 1;
+//     while(decNum > 0){
+//         int rem = decNum % 2;
+//         decNum = decNum / 2;
+//         ans += (rem * pow);
+//         pow = pow * 10;
+//     }
+//     return ans;
+// }
+// int main(){
+
+//     int decNum = 5;
+//     for (int i =1 ; i <= 10; i++){
+//         cout << decToBinary(i) << endl ;
+//     }
+//     return 0;
+// }
+
+// Binary to Decimal
+int binaryToDec(int binary){
     int ans = 0;
     int pow = 1;
-    while(decNum > 0){
-        int rem = decNum % 2;
-        decNum = decNum / 2;
+    while(binary > 0){
+        int rem = binary % 10 ;
         ans += (rem * pow);
-        pow = pow * 10;
+        binary /= 10;
+        pow *= 2;
     }
     return ans;
 }
-
 int main(){
 
-    int decNum = 5;
-    for (int i =1 ; i <= 10; i++){
-        cout << decToBinary(i) << endl ;
-    }
+    int binary = 111;
+    cout << binaryToDec(binary) << endl ;
     return 0;
 }

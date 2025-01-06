34259012d6082b9ecb1bfaac6d7eea8a0f13bc76
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-06 15:00:13 +0530

    Bitwise

--- bitwise.cpp
+++ bitwise.cpp
@@ -13,40 +13,60 @@ using namespace std;
 //     return 0;
 // }
 
-#include <iostream>
-using namespace std;
+// // Function to check if a number is a power of 2 using loops
+// bool isPowerOfTwoLoop(int n) {
+//     if (n <= 0) return false;
+//     while (n % 2 == 0) {
+//         n /= 2;
+//     }
+//     return n == 1;
+// }
 
-// Function to check if a number is a power of 2 using loops
-bool isPowerOfTwoLoop(int n) {
-    if (n <= 0) return false;
-    while (n % 2 == 0) {
-        n /= 2;
+// // Function to check if a number is a power of 2 using bitwise operations
+// bool isPowerOfTwoBitwise(int n) {
+//     if (n <= 0) return false;
+//     return (n & (n - 1)) == 0;
+// }
+
+// int main() {
+//     int num = 34;
+
+//     // Using loop-based method
+//     if (isPowerOfTwoLoop(num)) {
+//         cout << num << " is a power of 2 (using loop)." << endl;
+//     } else {
+//         cout << num << " is not a power of 2 (using loop)." << endl;
+//     }
+
+//     // Using bitwise method
+//     if (isPowerOfTwoBitwise(num)) {
+//         cout << num << " is a power of 2 (using bitwise)." << endl;
+//     } else {
+//         cout << num << " is not a power of 2 (using bitwise)." << endl;
+//     }
+
+//     return 0;
+// }
+
+// Function to reverse an integer
+int reverseInteger(int n) {
+    int reversed = 0;
+    while (n != 0) {
+        int digit = n % 10;
+        reversed = reversed * 10 + digit;
+        n /= 10;
     }
-    return n == 1;
-}
-
-// Function to check if a number is a power of 2 using bitwise operations
-bool isPowerOfTwoBitwise(int n) {
-    if (n <= 0) return false;
-    return (n & (n - 1)) == 0;
+    return reversed;
 }
 
 int main() {
-    int num = 34;
+    int num = 12345;
 
-    // Using loop-based method
-    if (isPowerOfTwoLoop(num)) {
-        cout << num << " is a power of 2 (using loop)." << endl;
-    } else {
-        cout << num << " is not a power of 2 (using loop)." << endl;
-    }
-
-    // Using bitwise method
-    if (isPowerOfTwoBitwise(num)) {
-        cout << num << " is a power of 2 (using bitwise)." << endl;
-    } else {
-        cout << num << " is not a power of 2 (using bitwise)." << endl;
-    }
+    // Reverse the integer
+    int reversedNum = reverseInteger(num);
+    cout << "Original number: " << num << endl;
+    cout << "Reversed number: " << reversedNum << endl;
 
     return 0;
 }
+

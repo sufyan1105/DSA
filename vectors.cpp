7eb2b6d081ea3c2d5f420f1d6cabdc3cbcb36580
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-09 12:59:08 +0530

    Vectors

--- vectors.cpp
+++ vectors.cpp
@@ -4,8 +4,11 @@ using namespace std;
 
 int main(){
 
-    vector<int> vec = {1, 2, 3, 4, 5};
-    cout << vec[0] << endl;
-    cout << vec[1] << endl;
+    // vector<int> vec = {1, 2, 3, 4, 5};
+    // cout << vec[0] << endl;
+    // cout << vec[1] << endl; 
+
+    
+
     return 0;
 }

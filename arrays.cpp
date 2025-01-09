077c01c16b6f2d6c6e1e667f49c19ddc2a4c3227
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-09 11:35:35 +0530

    intersection

--- arrays.cpp
+++ arrays.cpp
@@ -162,25 +162,46 @@ using namespace std;
 // }
 
 // WAF to print all the unique elements of an array
-void unique(int arr[], int size){
-    for (int i = 0; i < size; i++){
-        bool isunique = true;
-        for (int j = 0; j < size; j++){
-            if (i != j && arr[i] == arr[j]){
-                isunique = false;
+// void unique(int arr[], int size){
+//     for (int i = 0; i < size; i++){
+//         bool isunique = true;
+//         for (int j = 0; j < size; j++){
+//             if (i != j && arr[i] == arr[j]){
+//                 isunique = false;
+//                 break;
+//             }
+//         }
+//         if (isunique){
+//             cout << arr[i] << " ";
+//         }
+//     }
+// }
+
+// int main(){
+//     int arr[] = {1,2,3,4,5,1,2,3,4,7};
+//     int size = 10;
+//     unique(arr,size);
+//     cout << endl;
+//     return 0;
+// }
+
+// WAF to find intersection of two arrays
+void intersection(int arr1[], int size1, int arr2[], int size2){
+    for (int i = 0; i < size1; i++){
+        for (int j = 0; j < size2; j++){
+            if (arr1[i] == arr2[j]){
+                cout << arr1[i] << " ";
                 break;
             }
         }
-        if (isunique){
-            cout << arr[i] << " ";
-        }
     }
 }
-
 int main(){
-    int arr[] = {1,2,3,4,5,1,2,3,4,7};
-    int size = 10;
-    unique(arr,size);
+    int arr1[] = {1,2,3,4,5};
+    int arr2[] = {3,4,5,6,7};
+    int size1 = 5;
+    int size2 = 5;
+    intersection(arr1,size1,arr2,size2);
     cout << endl;
     return 0;
 }

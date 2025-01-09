1d48acaa54ba756eb814080903a46f1834900c35
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-09 11:33:37 +0530

    arranging folder

--- arrays.cpp
+++ arrays.cpp
@@ -133,30 +133,54 @@ using namespace std;
 // }
 
 // WAF to swap the maximum and minimum element of an array
-void swapmaxmin(int arr[], int size){
-    int max = INT_MIN;
-    int min = INT_MAX;
-    int maxindex = -1;
-    int minindex = -1;
+// void swapmaxmin(int arr[], int size){
+//     int max = INT_MIN;
+//     int min = INT_MAX;
+//     int maxindex = -1;
+//     int minindex = -1;
+//     for (int i = 0; i < size; i++){
+//         if (arr[i] > max){
+//             max = arr[i];
+//             maxindex = i;
+//         }
+//         if (arr[i] < min){
+//             min = arr[i];
+//             minindex = i;
+//         }
+//     }
+//     swap(arr[maxindex],arr[minindex]);
+// }
+// int main(){
+//     int arr[] = {1,2,3,4,5};
+//     int size = 5;
+//     swapmaxmin(arr,size);
+//     for (int i = 0; i < size; i++){
+//         cout << arr[i] << " ";
+//     }
+//     cout << endl;
+//     return 0;
+// }
+
+// WAF to print all the unique elements of an array
+void unique(int arr[], int size){
     for (int i = 0; i < size; i++){
-        if (arr[i] > max){
-            max = arr[i];
-            maxindex = i;
+        bool isunique = true;
+        for (int j = 0; j < size; j++){
+            if (i != j && arr[i] == arr[j]){
+                isunique = false;
+                break;
+            }
         }
-        if (arr[i] < min){
-            min = arr[i];
-            minindex = i;
+        if (isunique){
+            cout << arr[i] << " ";
         }
     }
-    swap(arr[maxindex],arr[minindex]);
 }
+
 int main(){
-    int arr[] = {1,2,3,4,5};
-    int size = 5;
-    swapmaxmin(arr,size);
-    for (int i = 0; i < size; i++){
-        cout << arr[i] << " ";
-    }
+    int arr[] = {1,2,3,4,5,1,2,3,4,7};
+    int size = 10;
+    unique(arr,size);
     cout << endl;
     return 0;
 }

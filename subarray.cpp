19f4aafdb5c9361f27cb21b664db6e3059e9d3cc
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-10 11:07:17 +0530

    max subarray sum

--- subarray.cpp
+++ subarray.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 using namespace std;
 
+// Function to print all subarrays
 // int main() {
 //     int n = 5;
 //     int arr[5] = {1, 2, 3, 4, 5};
@@ -16,3 +17,19 @@ using namespace std;
 //     }
 //     return 0;
 // }
+
+// MAximum Subarray Sum
+int main(){
+    int n = 5;
+    int arr[5] = {1, 2, 3, 4, 5};
+    int maxSum = INT_MIN;
+    for (int start = 0; start < n; start++) {
+        int sum = 0;
+        for (int end = start; end < n; end++) {
+            sum += arr[end];
+            maxSum = max(maxSum, sum);
+        }
+    }
+    cout << maxSum << endl;
+    return 0;
+}

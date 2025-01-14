693e69ff6331172e8d367c92e7e628a2c01954ca
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-14 12:04:58 +0530

    Pass by reference

--- pointers.cpp
+++ pointers.cpp
@@ -2,6 +2,13 @@
 #include <vector>
 using namespace std;
 
+// void changeA(int a){ // pass by value
+//     a = 20;
+// }
+void changeA(int* ptr){
+    *ptr = 20;
+}
+
 int main(){
 
     // int a = 10; 
@@ -13,8 +20,13 @@ int main(){
     // cout<< **ptr2 << endl;
 
     // Null Pointer
-    int* ptr = NULL;
-    cout<< ptr << endl;
+    // int* ptr = NULL;
+    // cout<< ptr << endl; // 0x0
+
+    // Pass by reference
+    int a = 10;
+    changeA(&a);
+    cout<< a << endl;
 
     return 0;
 }

deceb35fd1193c6f4c80dfd0d24c42b4871015a2
Sufyan Kadiwala <kadiwalasufyan03@gmail.com>
2025-01-18 11:00:55 +0530

    Binary Search Algorithm

---
+++ Binary_Search.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int binarySearch(vector<int> arr, int tar){
+    start = 0;
+    end = arr.size() - 1;
+    while(start <= end)
+}
+
+int main(){
+
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int tar1 = 9;
+
+    vector<int> arr = {1, 2, 3, 4, 5, 6, 7, 8, 9,10};
+    int tar2 = 10;
+
+    
+    
+
+
+    return 0;
+}
